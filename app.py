adb4129e009fed72b2208c24e0521be989f271d7
m-adamek <236731@student.uek.krakow.pl>
2026-04-13 02:16:41 +0200

    some logic transfered from the jupyter notebook

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, jsonify
+from logic import ghp, mrp
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DZIALA"
+
+@app.route("/ghp", methods=["POST"])
+def ghp_api():
+    data = request.json
+
+    produkcja, dostępne = ghp(
+        data["popyt"],
+        data["na_stanie"],
+        data.get("partia", None)
+    )
+
+    return jsonify({
+        "produkcja": produkcja,
+        "dostępne": dostępne
+    })
+
+
+@app.route("/mrp", methods=["POST"])
+def mrp_api():
+    data = request.json
+
+    wynik = mrp(
+        data["zapotrzebowanie"],
+        data["na_stanie"],
+        data["czas_realizacji"],
+        data["partia"]
+    )
+
+    return jsonify(wynik)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ logic.py
@@ -0,0 +1,62 @@
+import math
+
+# jeżeli produkcja to zapotrzebowanie netto to..:
+
+def ghp(przewidywany_popyt, na_stanie, wielkość_partii):
+    tydzień = len(przewidywany_popyt)
+    produkcja = [0] * tydzień
+    dostępne = [0] * tydzień
+    poprzednio_dostępne = na_stanie
+
+    for i in range(tydzień):
+        if poprzednio_dostępne >= przewidywany_popyt[i]:
+            produkcja[i] = 0
+        else:
+            zapotrzebowanie_netto = przewidywany_popyt[i] - poprzednio_dostępne
+            produkcja[i] = zapotrzebowanie_netto
+
+        dostępne[i] = poprzednio_dostępne + produkcja[i] - przewidywany_popyt[i]
+        poprzednio_dostępne = dostępne[i]
+
+    return produkcja, dostępne
+
+
+def mrp(całkowite_zapotrzebowanie, na_stanie, czas_realizacji, wielkość_partii):
+    tydzień = len(całkowite_zapotrzebowanie)
+
+    planowane_przyjęcia = [0] * tydzień
+    przewidywane_na_stanie = [0] * tydzień
+    zapotrzebowanie_netto = [0] * tydzień
+    planowane_przyjęcie_zamówień = [0] * tydzień
+    planowane_zamówienia = [0] * tydzień
+
+    poprzedni_stan = na_stanie
+
+    for i in range(tydzień):
+        przewidywane_na_stanie[i] = poprzedni_stan + planowane_przyjęcia[i] + planowane_przyjęcie_zamówień[i] - całkowite_zapotrzebowanie[i]
+
+
+        if przewidywane_na_stanie[i] < 0:
+            zapotrzebowanie_netto[i] = abs(przewidywane_na_stanie[i])      #abs(-15) = 15
+
+            ilość = math.ceil(zapotrzebowanie_netto[i] / wielkość_partii) * wielkość_partii
+
+            planowane_przyjęcie_zamówień[i] = ilość
+            przewidywane_na_stanie[i] += ilość
+
+            if i - czas_realizacji >= 0:
+                planowane_zamówienia[i - czas_realizacji] = ilość
+
+        poprzedni_stan = przewidywane_na_stanie[i]
+
+    return {
+        "Całkowite zapotrzebowanie": całkowite_zapotrzebowanie,
+        "Planowane przyjęcia": planowane_przyjęcia,
+        "Przewidywane na stanie": przewidywane_na_stanie,
+        "Zapotrzebowanie netto": zapotrzebowanie_netto,
+        "Planowane przyjęcie zamówień": planowane_przyjęcie_zamówień,
+        "Planowane zamówienia": planowane_zamówienia
+    }
+
+
+# def full_mrp(popyt, bom, zapasy, czasy, partie):   -> ?? 
